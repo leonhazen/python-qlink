b0137020f8fb62462b13f1814272ca2df40cb7e8
Leon Hazen <reon@reon.co>
2020-08-18 18:59:09 +1000

    Scaffolding serverless API functions

---
+++ api/api.py
@@ -0,0 +1,23 @@
+import json
+import logging
+
+def create(event, context):
+    # Placeholder
+    return {
+        'statusCode': 200,
+        'body': 'Create function'
+    }
+
+def get(event, context):
+    # Placeholder
+    return {
+        'statusCode': 200,
+        'body': 'Get function'
+    }
+
+def redir(event, context):
+    # Placeholder
+    return {
+        'statusCode': 200,
+        'body': 'Redirect function'
+    }
