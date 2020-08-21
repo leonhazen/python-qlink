ff2d8b1827c79bbc45096fa994f73dfed2d94cfe
Leon Hazen <reon@reon.co>
2020-08-21 23:11:00 +1000

    Define PynamoDB model

---
+++ api/model.py
@@ -0,0 +1,15 @@
+import os
+from datetime import datetime
+
+from pynamodb.attributes import UnicodeAttribute, BooleanAttribute, UTCDateTimeAttribute
+from pynamodb.models import Model
+
+class UrlModel(Model):
+    class Meta:
+        table_name = os.environ('DYNAMODB_TABLE')
+        region = os.environ('AWS_REGION')
+        host = 'https://dynamodb.' + region + '.amazonaws.com'
+
+    id = UnicodeAttribute(hash_key=True, null=False)
+    url = UnicodeAttribute(null=False)
+    created = UTCDateTimeAttribute(null=False, default=datetime.now())
