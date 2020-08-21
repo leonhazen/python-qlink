85899115564f8d3e009c14e18258ccf1aaecaf61
Leon Hazen <reon@reon.co>
2020-08-21 23:28:56 +1000

    Create request error handling

--- api/api.py
+++ api/api.py
@@ -1,7 +1,31 @@
 import json
 import logging
 
+from validator_collection import checkers
+
 def create(event, context):
+    data = json.loads(event['body'])
+    if 'url' not in data:
+        logging.error('URL parameter not provided')
+        return {
+            'statusCode': 422,
+            'body': json.dumps({'error_message': 'Insufficient data'})
+        }
+
+    if not data['url']:
+        logging.error('URL value missing')
+        return {
+            'statusCode': 422,
+            'body': json.dumps({'error_message': 'URL missing'})
+        }
+
+    if !checkers.is_url(data['url']):
+        logging.error('URL is invalid')
+        return {
+            'statusCode': 422,
+            'body': json.dumps({'error_message': 'URL invalid'})
+        }
+
     # Placeholder
     return {
         'statusCode': 200,
